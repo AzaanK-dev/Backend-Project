e43f33ab7addd9112931e66bf2287af35d92e301
AzaanK-dev <azaanmehtabk@gmail.com>
2026-05-24 01:03:17 +0500

    adding more routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, renewAccessToken } from "../controllers/user.controller.js";
+import { changePassword, getChannel, getWatchHistory, loginUser, logoutUser, registerUser, renewAccessToken, updateAccountDetails, updateAvatar, updateCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([     // file uploading
+    upload.fields([     // file uploading through multer
         {name: "avatar",maxCount : 1},
         {name: "coverImage",maxCount : 1}
     ])
@@ -17,5 +17,14 @@ router.route("/login").post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJwt,logoutUser)
 router.route("/refresh-token").post(renewAccessToken)
+
+router.route("change-password").patch(verifyJwt,changePassword)    // patch -> to change only specific patch of details
+router.route("update-account-details").patch(verifyJwt,updateAccountDetails)  
+
+router.route("/avatar").patch(verifyJwt,upload.single("avatar"),updateAvatar)  // upload middleware bcz file is required from user
+router.route("/coverImage").patch(verifyJwt,upload.single("coverImage"),updateCoverImage)
+
+router.route("/channel/:username").get(verifyJwt,getChannel)    // bcz req.params is used in this function so data from link is required
+router.route("/history").get(verifyJwt,getWatchHistory)     // get -> bcz no data from user is required
 
 export default router;
