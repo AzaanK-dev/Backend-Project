12a86332495c9fb0e4a748eed51b894c538518ae
AzaanK-dev <azaanmehtabk@gmail.com>
2026-05-30 12:23:20 +0500

    adding updateVideo and deleteVideo  controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -3,8 +3,7 @@ import ApiError from "../utils/ApiError.js"
 import { Video } from "../models/video.model.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 import ApiResponse from "../utils/ApiResponse.js";
-
-// https://github.com/hiteshchoudhary/chai-backend/blob/main/
+import {v2 as cloudinary} from "cloudinary";
 
 const uploadVideo = asyncHandler(async (req,res)=>{
     const {title,description} = req.body
@@ -42,6 +41,46 @@ const getVideoById = asyncHandler(async (req,res)=>{
     const video = await Video.findById(videoId);
     if(!video) throw new ApiError(404,"Video does not found!");
     res.status(200).json(new ApiResponse(200,video,"Video fetched succesfully"))
+})
+
+const updateVideo = asyncHandler(async (req,res)=>{
+    const {videoId} = req.params;
+    const {title,description} = req.body;
+
+    if(!videoId) throw new ApiError(400,"Video Id is required!")
+    if(!title || !description) throw new ApiError(400,"Required fields are empty!")
+    
+    const thumbnailPath = req.files?.thumbnail[0]?.path;
+    const thumbnail = await uploadOnCloudinary(thumbnailPath)
+
+    const updateFields = {
+        title,
+        description,
+    }
+    if(thumbnail) updateFields.thumbnail = thumbnail.url;
+
+    const video = Video.findByIdAndUpdate(videoId,updateFields,{new:true});
+    res.status(200).json(new ApiResponse(200,video,"Video updated successfully"))
+})
+
+const deleteVideo = asyncHandler(async (req,res)=>{
+    const {videoId} = req.params;
+    const video = await Video.findByIdAndDelete(videoId);
+    if(!video) throw new ApiError(404,"Video not found")
+        
+    const videoFileId = video.videoFile.url.split("/",7)
+    cloudinary.uploader.destroy(videoFileId,{
+        resource_type: "video"
+    })
+    
+    if(video.thumbnail){
+        const thumbnailId = video.thumbnail.url.split("/",7)
+        cloudinary.uploader.destroy(thumbnailId,{
+            resource_type: "image"
+        })
+    }
+        
+    res.status(200).json(new ApiResponse(200,{},"Video deleted successfully"))
 })
 
 export{
