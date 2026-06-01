777c2287e659e7643bab961bbed567493928b4a0
AzaanK-dev <azaanmehtabk@gmail.com>
2026-06-02 00:16:28 +0500

    adding objectId validation in video enpoints

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { deleteVideo, getAllVideos, getVideoById, updateVideo, uploadVideo } from "../controllers/video.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { validateObjectId } from "../middlewares/validateObjectId.middleware.js"
 
 const router = Router()
 router.use(verifyJwt) // Apply verifyJWT to all routes in this file
@@ -9,12 +10,14 @@ router.use(verifyJwt) // Apply verifyJWT to all routes in this file
 router.route("/upload").post(upload.fields([
     {name:"videoFile", maxCount:1},
     {name:"thumbnail", maxCount:1}
-]),  uploadVideo)
+]),uploadVideo)
 
 router.route("/").get(getAllVideos)
-router.route("/:videoId").get(getVideoById)
-router.route("/update/:videoId").patch(updateVideo)
-router.route("/delete/:videoId").get(deleteVideo)
+router.route("/:videoId").get(validateObjectId("videoId"),getVideoById)
+
+router.route("/video/:videoId")
+.patch(validateObjectId("videoId"),updateVideo)
+.delete(validateObjectId("videoId"),deleteVideo)
 
 
-export default router
+export default router;
